067c77593c1405c679898bfa0410d24dff27894d
judyhe <judy.w.he@gmail.com>
2010-09-26 23:00:54 -0400

    add test for home page, add teaser homepage

---
+++ spec/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+  
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+end
