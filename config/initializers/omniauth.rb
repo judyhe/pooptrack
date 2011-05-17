3caf532bce8786aa777002ea85bf926f7a9f110e
judyhe <judy.w.he@gmail.com>
2011-05-16 22:18:43 -0400

    make return path be https

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 require 'openid/store/filesystem'
 
+OmniAuth.config.full_path = "https://www.pooptrack.com"
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id, OpenID::Store::Filesystem.new('./tmp')
   use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
